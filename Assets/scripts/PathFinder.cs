514c52ef282682fa117bf50e4f8d43faf6014738
westin <westin007@gmail.com>
2014-09-08 20:02:42 -0500

    pathfinder - created thing that will do traversal - Mr.B

---
+++ Assets/scripts/PathFinder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using ultraviolet.builder;
+
+namespace ultraviolet.actor
+{
+    public class PathFinder : MonoBehaviour
+    {
+
+        public Cell currentCell;
+        public Cell targetCell;
+
+        void Start()
+        {
+            if (currentCell == null)
+            {
+                throw new Exception("This actor must start at a cell.  Please assign currentCell.");
+            }
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
